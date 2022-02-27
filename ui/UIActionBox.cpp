00d7c00feb082133a4ea44ea9799ec1ec7841943
Gaoran Sun <33003517+shb3014@users.noreply.github.com>
2022-02-27 21:31:48 +0800

    adding ui action box

---
+++ ui/UIActionBox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sungaoran on 2022/2/27.
+//
+
+#include "UIActionBox.h"
---
+++ ui/UIActionBox.h
@@ -0,0 +1,18 @@
+//
+// Created by sungaoran on 2022/2/27.
+//
+
+#ifndef LVGL_UIACTIONBOX_H
+#define LVGL_UIACTIONBOX_H
+
+#include "UIBase.h"
+
+class UIActionBox: public UIBase{
+public:
+    void show() override;
+
+//    void update(const char* message, )
+};
+
+
+#endif //LVGL_UIACTIONBOX_H
