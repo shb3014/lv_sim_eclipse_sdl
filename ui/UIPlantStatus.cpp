0f963ed58f887551527a4c89818fd41d1e047b87
Gaoran Sun <shb3014@126.com>
2022-04-19 14:00:46 +0800

    adding animation

--- ui/UIPlantStatus.cpp
+++ ui/UIPlantStatus.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "UIPlantStatus.h"
+#include "tools.h"
 
 StatusCard::StatusCard(lv_obj_t *parent, const char *name, const char *unit) {
     m_card = lv_obj_create(parent);
@@ -34,54 +35,186 @@ void StatusCard::update_value_label(const char *value_str) {
     lv_obj_set_style_text_color(m_value_label, lv_color_white(), 0);
 }
 
-UIPlantStatus::UIPlantStatus()
-        : UIBase(),
-          bubble(m_scr, 300,100,20, 10),
-          card_temp(m_scr, "temp", "°C"),
-          card_humidity(m_scr, "humidity", "%"),
-          card_soil(m_scr, "soil", "%"),
-          card_water(m_scr, "water", "mL"),
-          card_battery(m_scr, "battery", "%"),
-          card_light(m_scr, "light", "lx") {
-    lv_obj_set_flex_flow(m_scr, LV_FLEX_FLOW_ROW_WRAP);
-    lv_obj_set_flex_align(m_scr, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+ChatBubble::ChatBubble(lv_obj_t *parent, lv_coord_t bubble_w, lv_coord_t bubble_h, lv_coord_t pointer_w,
+                       lv_coord_t pointer_h)
+        : m_space(lv_obj_create(parent)),
+          m_pointer(m_space, pointer_w, pointer_h) {
+    lv_obj_remove_style_all(m_space);
+    lv_obj_set_size(m_space, bubble_w, bubble_h + pointer_h * 2);
 
-}
-
-ChatBubble::ChatBubble(lv_obj_t *parent, int16_t bubble_w, int16_t bubble_h, int16_t pointer_w, int16_t pointer_h,
-                       lv_color_t color)
-        : m_pointer(parent, pointer_w, pointer_h),
-          m_color(color) {
-    m_card = lv_obj_create(parent);
+    m_card = lv_obj_create(m_space);
+    lv_obj_set_style_bg_opa(m_card, LV_OPA_TRANSP, 0);
     lv_obj_set_size(m_card, bubble_w, bubble_h);
+    lv_obj_align(m_card, LV_ALIGN_CENTER, 0, 0);
+
     m_label = lv_label_create(m_card);
+    lv_obj_set_style_text_color(m_card, lv_color_white(), 0);
+
     update_pointer_location(20);
+    m_pointer.update_color(lv_color_white());
+    lv_obj_move_foreground(m_pointer.body);
 }
 
 void ChatBubble::update_pointer_location(int x) {
-    lv_obj_align_to(m_pointer.body, m_card, LV_ALIGN_TOP_LEFT, x, 0);
+    lv_obj_align_to(m_pointer.body, m_card, LV_ALIGN_OUT_TOP_LEFT, x, 2);
 }
 
-BubblePointer::BubblePointer(lv_obj_t *parent, int16_t pointer_w, int16_t pointer_h)
-        : left_points({{0,                         pointer_h},
-                       {(int16_t) (pointer_w / 2), 0}}),
-          right_points({{(int16_t) (pointer_w / 2), 0},
-                        {pointer_w,                 pointer_h}}),
-          bottom_points({{0,         pointer_h},
-                         {pointer_w, pointer_h}}) {
+void ChatBubble::update_color(lv_color_t color) {
+    m_pointer.update_color(color);
+    lv_obj_set_style_border_color(m_card, color, 0);
+}
+
+void ChatBubble::update_content(const char *content) {
+    lv_label_set_text(m_label, content);
+}
+
+void ChatBubble::set_visible(bool show) {
+    if (show) {
+        lv_obj_clear_flag(m_space, LV_OBJ_FLAG_HIDDEN);
+    } else {
+        lv_obj_add_flag(m_space, LV_OBJ_FLAG_HIDDEN);
+    }
+    visible = show;
+}
+
+BubblePointer::BubblePointer(lv_obj_t *parent, lv_coord_t pointer_w, lv_coord_t pointer_h, lv_coord_t line_w)
+        : m_pointer_h(pointer_h),
+          m_pointer_w(pointer_w),
+          m_line_w(line_w) {
     body = lv_obj_create(parent);
     lv_obj_remove_style_all(body);
-    line_left = lv_line_create(body);
-    line_right = lv_line_create(body);
     line_bottom = lv_line_create(body);
+    line_top = lv_line_create(body);
     lv_obj_set_size(body, pointer_w, pointer_h);
-    lv_line_set_points(line_left, left_points.data(), 2);
-    lv_line_set_points(line_right, right_points.data(), 2);
-    lv_line_set_points(line_bottom, bottom_points.data(), 2);
+    lv_line_set_points(line_top, points_top.data(), 3);
+    lv_line_set_points(line_bottom, points_bottom.data(), 2);
+    lv_obj_set_style_line_width(line_top, line_w, 0);
+    lv_obj_set_style_line_width(line_bottom, line_w, 0);
 }
 
-void BubblePointer::update(lv_color_t color) {
-    lv_obj_set_style_line_color(line_left, color, 0);
-    lv_obj_set_style_line_color(line_right, color, 0);
+void BubblePointer::update_color(lv_color_t color) {
+    lv_obj_set_style_line_color(line_top, color, 0);
     lv_obj_set_style_line_color(line_bottom, lv_color_black(), 0);
+}
+
+void BubblePointer::set_direction(bool up) {
+    if (up) {
+        points_top = {{0,                              (lv_coord_t) (m_pointer_h - m_line_w / 2)},
+                      {(lv_coord_t) (m_pointer_w / 2), (lv_coord_t) (m_line_w / 2)},
+                      {m_pointer_w,                    (lv_coord_t) (m_pointer_h - m_line_w / 2)}};
+        points_bottom = {{0,                          (lv_coord_t) (m_pointer_h - m_line_w / 2)},
+                         {(lv_coord_t) (m_pointer_w), (lv_coord_t) (m_pointer_h - m_line_w / 2)}};
+    }else{
+        points_top = {{0,                              0},
+                      {(lv_coord_t) (m_pointer_w / 2), (lv_coord_t) (m_line_w / 2)},
+                      {m_pointer_w,                    (lv_coord_t) (m_pointer_h - m_line_w / 2)}};
+        points_bottom = {{0,                          (lv_coord_t) (m_pointer_h - m_line_w / 2)},
+                         {(lv_coord_t) (m_pointer_w), (lv_coord_t) (m_pointer_h - m_line_w / 2)}};
+
+    }
+
+}
+
+
+UIPlantStatus::UIPlantStatus()
+        : UIBase(),
+          top_area(lv_obj_create(m_scr)),
+          bottom_area(lv_obj_create(m_scr)),
+          bubble(m_scr, 300, 100, 20, 10),
+          card_temp(top_area, "temp", "°C"),
+          card_humidity(top_area, "humidity", "%"),
+          card_soil(top_area, "soil", "%"),
+          card_water(bottom_area, "water", "mL"),
+          card_battery(bottom_area, "battery", "%"),
+          card_light(bottom_area, "light", "lx") {
+    lv_obj_clear_flag(m_scr, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_remove_style_all(top_area);
+    lv_obj_remove_style_all(bottom_area);
+    lv_obj_set_size(top_area, 320, 110);
+    lv_obj_set_size(bottom_area, 320, 110);
+
+    lv_obj_set_flex_flow(top_area, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(top_area, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+    lv_obj_set_flex_flow(bottom_area, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(bottom_area, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+//    lv_obj_set_flex_flow(m_scr, LV_FLEX_FLOW_COLUMN);
+//    lv_obj_set_flex_align(m_scr, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+    lv_obj_align(top_area, LV_ALIGN_TOP_MID, 0, 5);
+    lv_obj_align(bubble.m_space, LV_ALIGN_BOTTOM_MID, 0, -5);
+    lv_obj_align(bottom_area, LV_ALIGN_BOTTOM_MID, 0, -5);
+    bubble.set_visible(false);
+}
+
+static void anim_set_align(void *var, int32_t value) {
+    lv_obj_align((lv_obj_t *) var, LV_ALIGN_BOTTOM_MID, 0, value);
+}
+
+static void bubble_show_cb(struct _lv_anim_t *anim) {
+    auto *bubble = static_cast<ChatBubble *>(anim->user_data);
+    bubble->set_visible(true);
+    auto a = anim_create(bubble->m_space, anim_fade, LV_OPA_TRANSP, LV_OPA_COVER, 500);
+    lv_anim_start(&a);
+}
+
+void UIPlantStatus::hide_bubble_cb() {
+    bubble.set_visible(false);
+    auto a = anim_create(bottom_area, anim_set_align, 105, -5, 500);
+    lv_anim_start(&a);
+}
+
+
+static void bubble_hide_cb(struct _lv_anim_t *anim) {
+    auto *ui = static_cast<UIPlantStatus *>(anim->user_data);
+    ui->hide_bubble_cb();
+}
+
+void UIPlantStatus::update_bubble_status(const char *content, lv_color_t color, lv_coord_t x, bool show) {
+    if (show) {
+        bubble.update_pointer_location(x);
+        bubble.update_color(color);
+        bubble.update_content(content);
+        if (!bubble.visible) {
+            auto a = anim_create(bottom_area, anim_set_align, -5, 105, 300, 0, 0, 0, bubble_show_cb,
+                                 &bubble);
+            lv_anim_start(&a);
+        }
+    } else {
+        if (bubble.visible) {
+            auto a = anim_create(bubble.m_space, anim_fade, LV_OPA_COVER, LV_OPA_TRANSP, 300, 0, 0, 0, bubble_hide_cb,
+                                 this);
+            lv_anim_start(&a);
+        }
+    }
+}
+
+void UIPlantStatus::routine() {
+    static lv_color_t colors[] = {lv_color_white(), lv_color_make(255, 0, 0), lv_color_make(200, 200, 0)};
+    static lv_coord_t pos[] = {40, 130, 250};
+    static const char *contents[] = {"test1", "test2", "test3"};
+    static int i = 20;
+    UIBase::routine();
+    static int last_index = -1;
+    int new_index = 0;
+    i++;
+    if (i > 80) {
+        update_bubble_status("", lv_color_white(), 20, false);
+        i = 0;
+        return;
+    } else if (i > 60) {
+        new_index = 2;
+    } else if (i > 40) {
+        new_index = 1;
+    } else if (i > 20) {
+        new_index = 0;
+    } else {
+
+        return;
+    }
+    if (new_index != last_index) {
+        last_index = new_index;
+        update_bubble_status(contents[new_index], colors[new_index], pos[new_index], true);
+    }
 }
--- ui/UIPlantStatus.h
+++ ui/UIPlantStatus.h
@@ -12,10 +12,11 @@ class StatusCard {
 public:
     StatusCard(lv_obj_t *parent, const char *name, const char *unit);
 
+    lv_obj_t *m_card;
 private:
     void update_value_label(const char *value_str);
 
-    lv_obj_t *m_card;
+
     lv_obj_t *m_value_label;
     lv_obj_t *m_name_label;
     lv_obj_t *m_unit_label;
@@ -23,30 +24,40 @@ private:
 
 class BubblePointer {
 public:
-    BubblePointer(lv_obj_t *parent, int16_t pointer_w, int16_t pointer_h);
+    BubblePointer(lv_obj_t *parent, lv_coord_t pointer_w, lv_coord_t pointer_h, lv_coord_t line_w = 2);
 
-    void update(lv_color_t color = lv_color_white());
+    void update_color(lv_color_t color = lv_color_white());
+
+    void set_direction(bool up);
 
     lv_obj_t *body;
 
 private:
-    lv_obj_t *line_left;
-    lv_obj_t *line_right;
+    lv_coord_t m_pointer_w;
+    lv_coord_t m_pointer_h;
+    lv_coord_t m_line_w;
+    lv_obj_t *line_top;
     lv_obj_t *line_bottom;
-    std::vector<lv_point_t> left_points;
-    std::vector<lv_point_t> right_points;
-    std::vector<lv_point_t> bottom_points;
+    std::vector<lv_point_t> points_top;
+    std::vector<lv_point_t> points_bottom;
 };
 
 class ChatBubble {
 public:
-    ChatBubble(lv_obj_t *parent, int16_t bubble_w, int16_t bubble_h, int16_t pointer_w, int16_t pointer_h, lv_color_t color = lv_color_white());
+    ChatBubble(lv_obj_t *parent, lv_coord_t bubble_w, lv_coord_t bubble_h, lv_coord_t pointer_w, lv_coord_t pointer_h);
 
     void update_pointer_location(int x);
 
+    void update_color(lv_color_t color);
+
+    void update_content(const char *content);
+
+    void set_visible(bool show);
+
+    lv_obj_t *m_space;
+    bool visible = false;
 private:
     BubblePointer m_pointer;
-    lv_color_t m_color;
     lv_obj_t *m_card;
     lv_obj_t *m_label;
 };
@@ -59,15 +70,25 @@ public:
         return UI_PLANT_STATUS;
     }
 
+    void update_bubble_status(const char *content, lv_color_t color, lv_coord_t x, bool show);
+
+    void hide_bubble_cb();
+
+    void routine() override;
+
 private:
+//    lv_coord_t col_dsc[4] = {100, 100, 100, LV_GRID_TEMPLATE_LAST};
+//    lv_coord_t row_dsc[4] = {110, 110, 110, LV_GRID_TEMPLATE_LAST};
+    lv_obj_t *top_area;
+    ChatBubble bubble;
+    lv_obj_t *bottom_area;
     StatusCard card_temp;
     StatusCard card_humidity;
     StatusCard card_soil;
     StatusCard card_water;
     StatusCard card_battery;
     StatusCard card_light;
-    ChatBubble bubble;
-//    lv_obj_t *message_label;
+
 };
 
 
